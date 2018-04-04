e6093bf578fb0e0b2243754ead3e346cf1f7b11a
Florentin <flo.noel@hotmail.fr>
2018-04-04 02:46:23 +0200

    Correction fragment / amélioration carte
    Clé api cachée
    Simplification du fragment
    Changement des marqueurs de la carte
    Ajout des infos de la stations des markers et préparation du listener

--- app/src/main/java/fr/fnoel/tapandgo/activities/MapsActivity.java
+++ app/src/main/java/fr/fnoel/tapandgo/activities/MapsActivity.java
@@ -1,108 +1,97 @@
 package fr.fnoel.tapandgo.activities;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+import fr.fnoel.tapandgo.R;
+import fr.fnoel.tapandgo.fragment.DataFragment;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import fr.fnoel.tapandgo.fragment.DataFragment;
-import fr.fnoel.tapandgo.R;
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+    DataFragment.OnFragmentInteractionListener {
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, DataFragment.OnFragmentInteractionListener {
+  private static final String TAG = "MAPS_ACTIVITY";
+  private GoogleMap mMap;
+  private JSONArray listeStation = new JSONArray();
 
-	private GoogleMap mMap;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_maps);
+    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        .findFragmentById(R.id.map);
+    mapFragment.getMapAsync(this);
 
-	private JSONArray listeStation = new JSONArray();
-	private DataFragment mDataFragment;
+    String api_key = getResources().getString(R.string.jcdecaux_key);
+    DataFragment mDataFragment = DataFragment.getInstance(getSupportFragmentManager());
+    mDataFragment.startFetching(api_key);
 
-	BitmapDescriptor closeIcon;
-	BitmapDescriptor availableIcon;
-	BitmapDescriptor unavailableIcon;
-
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_maps);
-		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
-		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.map);
-		mapFragment.getMapAsync(this);
-
-		closeIcon = BitmapDescriptorFactory.fromResource(R.drawable.close);
-		availableIcon = BitmapDescriptorFactory.fromResource(R.drawable.available);
-		unavailableIcon = BitmapDescriptorFactory.fromResource(R.drawable.unavailable);
-
-		mDataFragment = DataFragment.getInstance(getSupportFragmentManager(), listeStation);
-		if (mDataFragment != null) {
-			// Execute the async download.
-			mDataFragment.startFetching();
-		}
-
-		// GOTO FILTER VIEW
-		// 		startActivity(new Intent(this, MapsActivity.class));
-	}
+    // GOTO FILTER VIEW
+    // 		startActivity(new Intent(this, MapsActivity.class));
+  }
 
 
-	/**
-	 * Manipulates the map once available.
-	 * This callback is triggered when the map is ready to be used.
-	 * This is where we can add markers or lines, add listeners or move the camera. In this case,
-	 * we just add a marker near Sydney, Australia.
-	 * If Google Play services is not installed on the device, the user will be prompted to install
-	 * it inside the SupportMapFragment. This method will only be triggered once the user has
-	 * installed Google Play services and returned to the app.
-	 */
-	@Override
-	public void onMapReady(GoogleMap googleMap) {
-		mMap = googleMap;
-		LatLng nantes = new LatLng(47.214617, -1.549949);
-		mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(nantes, 8.0f));
-	}
+  @Override
+  public void onMapReady(GoogleMap googleMap) {
+    mMap = googleMap;
+    LatLng nantes = new LatLng(47.214617, -1.549949);
+    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(nantes, 13.0f));
+
+    mMap.setOnMarkerClickListener(new OnMarkerClickListener() {
+      @Override
+      public boolean onMarkerClick(Marker marker) {
+        JSONObject station = (JSONObject) marker.getTag();
+        Log.i(TAG, "onMarkerClick: " + station);
+        // TODO Externaliser listener / afficher bottom sheet
+        return false;
+      }
+    });
+  }
 
 
-	@Override
-	public void onFragmentInteraction(String json) {
-		try {
-			Log.i("MainActivity", json);
-			listeStation = new JSONArray(json);
-			initLocation();
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-	}
+  @Override
+  public void onFragmentInteraction(String json) {
+    try {
+      listeStation = new JSONArray(json);
+      initLocation();
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+  }
 
-	private void initLocation() throws JSONException {
-		for (int i = 0; i < listeStation.length(); i++) {
-			JSONObject station = listeStation.getJSONObject(i);
-			JSONObject pos = station.getJSONObject("position");
-			LatLng coordStation = new LatLng(pos.getDouble("lat"), pos.getDouble("lng"));
+  private void initLocation() throws JSONException {
+    for (int i = 0; i < listeStation.length(); i++) {
+      JSONObject station = listeStation.getJSONObject(i);
+      JSONObject pos = station.getJSONObject("position");
+      LatLng coordStation = new LatLng(pos.getDouble("lat"), pos.getDouble("lng"));
 
-			String status = station.getString("status");
-			int availableBike = station.getInt("available_bikes");
+      String status = station.getString("status");
+      int availableBike = station.getInt("available_bikes");
 
-			if ("OPEN".equals(status)) {
-				if (availableBike == 0) {
-					mMap.addMarker(new MarkerOptions().position(coordStation).icon(unavailableIcon));
+      if ("OPEN".equals(status)) {
+        if (availableBike == 0) {
+          mMap.addMarker(new MarkerOptions().position(coordStation)
+              .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))).setTag(station);
+        } else {
+          mMap.addMarker(new MarkerOptions().position(coordStation)
+              .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))).setTag(station);
+        }
+      } else {
+        mMap.addMarker(new MarkerOptions().position(coordStation)
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE))).setTag(station);
+      }
+    }
+  }
 
-				} else {
-					mMap.addMarker(new MarkerOptions().position(coordStation).icon(availableIcon));
-				}
-			} else {
-				mMap.addMarker(new MarkerOptions().position(coordStation).icon(closeIcon));
-			}
-		}
-	}
 }
--- app/src/main/java/fr/fnoel/tapandgo/fragment/DataFragment.java
+++ app/src/main/java/fr/fnoel/tapandgo/fragment/DataFragment.java
@@ -1,231 +1,194 @@
 package fr.fnoel.tapandgo.fragment;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
+import fr.fnoel.tapandgo.R;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
-
-import fr.fnoel.tapandgo.R;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DataFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link DataFragment#getInstance} factory method to
- * create an instance of this fragment.
+ * A simple {@link Fragment} subclass. Activities that contain this fragment must implement the {@link
+ * DataFragment.OnFragmentInteractionListener} interface to handle interaction events. Use the {@link
+ * DataFragment#getInstance} factory method to create an instance of this fragment.
  */
 public class DataFragment extends Fragment {
-	private static final String TAG = "DataFragment";
 
-	private static final String LISTE_KEY = "listeStation";
+  private static final String TAG = "DataFragment";
 
-	private static final String URL = "https://api.jcdecaux.com/vls/v1/stations?contract=Nantes&apiKey=";
-	private String api_key;
-	private OnFragmentInteractionListener mListener;
-	private DownloadTask mDownloadTask;
+  private static final String API_KEY = "API_KEY";
 
-	public DataFragment() {
-		// Required empty public constructor
-	}
+  private static final String URL = "https://api.jcdecaux.com/vls/v1/stations?contract=Nantes&apiKey=";
+  private OnFragmentInteractionListener mListener;
+  private DownloadTask mDownloadTask;
 
-	/**
-	 * Use this factory method to create a new instance of
-	 * this fragment using the provided parameters.
-	 *
-	 * @param listeStation liste des statios.
-	 * @return A new instance of fragment DataFragment.
-	 */
-	public static DataFragment getInstance(FragmentManager fragmentManager, JSONArray listeStation) {
-		DataFragment fragment = new DataFragment();
-		Bundle args = new Bundle();
-		args.putString(LISTE_KEY, listeStation.toString());
-		fragmentManager.beginTransaction().add(fragment, TAG).commit();
-		return fragment;
-	}
+  public DataFragment() {
+  }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		setRetainInstance(true);
-		super.onCreate(savedInstanceState);
+  /**
+   * Use this factory method to create a new instance of this fragment using the provided parameters.
+   *
+   * @return A new instance of fragment DataFragment.
+   */
+  public static DataFragment getInstance(FragmentManager fragmentManager) {
+    DataFragment fragment = new DataFragment();
+    fragmentManager.beginTransaction().add(fragment, TAG).commit();
+    return fragment;
+  }
 
-		if (getArguments() != null) {
-			try {
-				JSONArray listeStation = new JSONArray(getArguments().getString(LISTE_KEY));
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setRetainInstance(true);
+  }
 
 
-	@Override
-	public void onAttach(Context context) {
-		super.onAttach(context);
-		if (context instanceof OnFragmentInteractionListener) {
-			mListener = (OnFragmentInteractionListener) context;
-		} else {
-			throw new RuntimeException(context.toString()
-					+ " must implement OnFragmentInteractionListener");
-		}
-		api_key = context.getString(R.string.jcdecaux_key);
-	}
+  @Override
+  public void onAttach(Context context) {
+    super.onAttach(context);
 
-	@Override
-	public void onDetach() {
-		super.onDetach();
-		mListener = null;
-	}
+    if (context instanceof OnFragmentInteractionListener) {
+      mListener = (OnFragmentInteractionListener) context;
+    } else {
+      throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
+    }
+  }
 
-	public void startFetching() {
-		cancelFetching();
-		mDownloadTask = new DownloadTask(this);
-		mDownloadTask.execute(URL + api_key);
-	}
+  @Override
+  public void onDetach() {
+    super.onDetach();
+    mListener = null;
+  }
 
-	public void cancelFetching() {
-		if (mDownloadTask != null) {
-			mDownloadTask.cancel(true);
-		}
-	}
+  public void startFetching(String api_key) {
+    mDownloadTask = new DownloadTask(this);
+    mDownloadTask.execute(URL.concat(api_key));
+  }
+
+  public void cancelFetching() {
+    if (mDownloadTask != null) {
+      mDownloadTask.cancel(true);
+    }
+  }
 
 
-	/**
-	 * This interface must be implemented by activities that contain this
-	 * fragment to allow an interaction in this fragment to be communicated
-	 * to the activity and potentially other fragments contained in that
-	 * activity.
-	 * <p>
-	 * See the Android Training lesson <a href=
-	 * "http://developer.android.com/training/basics/fragments/communicating.html"
-	 * >Communicating with Other Fragments</a> for more information.
-	 */
-	public interface OnFragmentInteractionListener {
-		void onFragmentInteraction(String json);
-	}
+  /**
+   * This interface must be implemented by activities that contain this fragment to allow an interaction in this
+   * fragment to be communicated to the activity and potentially other fragments contained in that activity. <p> See the
+   * Android Training lesson <a href= "http://developer.android.com/training/basics/fragments/communicating.html"
+   * >Communicating with Other Fragments</a> for more information.
+   */
+  public interface OnFragmentInteractionListener {
 
-	/**
-	 * Implementation of AsyncTask designed to fetch data from the network.
-	 */
-	private class DownloadTask extends AsyncTask<String, Integer, DownloadTask.Result> {
+    void onFragmentInteraction(String json);
+  }
 
-		protected DataFragment fragment = null;
+  /**
+   * Implementation of AsyncTask designed to fetch data from the network.
+   */
+  private static class DownloadTask extends AsyncTask<String, Integer, DownloadTask.Result> {
 
-		DownloadTask(DataFragment fragment) {
-			this.fragment = fragment;
-		}
+    private DataFragment fragment;
 
-		/**
-		 * Wrapper class that serves as a union of a result value and an exception. When the download
-		 * task has completed, either the result value or exception can be a non-null value.
-		 * This allows you to pass exceptions to the UI thread that were thrown during doInBackground().
-		 */
-		class Result {
-			public String mResultValue;
-			public Exception mException;
+    DownloadTask(DataFragment fragment) {
+      this.fragment = fragment;
+    }
 
-			public Result(String resultValue) {
-				mResultValue = resultValue;
-			}
+    /**
+     * Wrapper class that serves as a union of a result value and an exception. When the download task has completed,
+     * either the result value or exception can be a non-null value. This allows you to pass exceptions to the UI thread
+     * that were thrown during doInBackground().
+     */
+    class Result {
 
-			public Result(Exception exception) {
-				mException = exception;
-			}
-		}
+      private String mResultValue;
+      private Exception mException;
 
-		/**
-		 * Cancel background network operation if we do not have network connectivity.
-		 */
-		@Override
-		protected void onPreExecute() {
+      private Result(String resultValue) {
+        mResultValue = resultValue;
+      }
+
+      private Result(Exception exception) {
+        mException = exception;
+      }
+    }
+
+    /**
+     * Cancel background network operation if we do not have network connectivity.
+     */
+    @Override
+    protected void onPreExecute() {
 //			if (fragment != null) {
 //				NetworkInfo networkInfo = fragment.getActiveNetworkInfo();
 //				if (networkInfo == null || !networkInfo.isConnected() ||
 //						(networkInfo.getType() != ConnectivityManager.TYPE_WIFI
 //								&& networkInfo.getType() != ConnectivityManager.TYPE_MOBILE)) {
-//					// If no connectivity, cancel task and update Callback with null data.
-//					fragment.updateFromDownload(null);
 //					cancel(true);
 //				}
 //			}
-		}
+    }
 
-		/**
-		 * Defines work to perform on the background thread.
-		 */
-		@Override
-		protected DownloadTask.Result doInBackground(String... urls) {
-			Result result = null;
-			if (!isCancelled() && urls != null && urls.length > 0) {
-				String urlString = urls[0];
-				try {
-					URL url = new URL(urlString);
-					String resultString = downloadUrl(url);
-					if (resultString != null) {
-						result = new Result(resultString);
-					} else {
-						throw new IOException("No response received.");
-					}
-				} catch (Exception e) {
-					result = new Result(e);
-				}
-			}
-			return result;
-		}
+    /**
+     * Defines work to perform on the background thread.
+     */
+    @Override
+    protected DownloadTask.Result doInBackground(String... urls) {
+      Result result = null;
+      if (!isCancelled() && urls != null && urls.length > 0) {
+        String urlString = urls[0];
+        try {
+          URL url = new URL(urlString);
+          String resultString = downloadUrl(url);
+          result = new Result(resultString);
+        } catch (Exception e) {
+          result = new Result(e);
+        }
+      }
+      return result;
+    }
 
-		private String downloadUrl(URL url) throws IOException {
-			StringBuilder result = new StringBuilder();
-			HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-			connection.setRequestMethod("GET");
-			connection.setReadTimeout(10000 /* milliseconds */);
-			connection.setConnectTimeout(15000 /* milliseconds */);
-			connection.setDoOutput(true);
-			connection.connect();
+    private String downloadUrl(URL url) {
+      StringBuilder result = new StringBuilder();
 
-			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+      try (InputStreamReader isr = new InputStreamReader(url.openStream())) {
+        try (BufferedReader br = new BufferedReader(isr)) {
+          String line;
+          while ((line = br.readLine()) != null) {
+            result.append(line + "\n");
+          }
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
 
-			String line;
-			while ((line = br.readLine()) != null) {
-				result.append(line + "\n");
-			}
-			br.close();
-			connection.disconnect();
-
-			return result.toString();
-		}
+      return result.toString();
+    }
 
 
-		/**
-		 * Updates the DownloadCallback with the result.
-		 */
-		@Override
-		protected void onPostExecute(Result result) {
-			if (result != null && fragment != null) {
-				if (result.mException != null) {
-//					fragment.updateFromDownload(result.mException.getMessage());
-				} else if (result.mResultValue != null) {
-					fragment.mListener.onFragmentInteraction(result.mResultValue);
-				}
-			}
-		}
+    /**
+     * Updates the DownloadCallback with the result.
+     */
+    @Override
+    protected void onPostExecute(Result result) {
+      if (result != null && fragment != null && result.mResultValue != null) {
+        fragment.mListener.onFragmentInteraction(result.mResultValue);
+      }
+    }
 
-		/**
-		 * Override to add special behavior for cancelled AsyncTask.
-		 */
-		@Override
-		protected void onCancelled(Result result) {
-		}
-	}
+    /**
+     * Override to add special behavior for cancelled AsyncTask.
+     */
+    @Override
+    protected void onCancelled(Result result) {
+    }
+  }
 
 }
